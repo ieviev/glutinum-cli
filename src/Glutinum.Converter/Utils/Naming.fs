1cb3f798759b63139c472384ddc766f5084e6869
ieviev <ieviev@users.noreply.github.com>
2025-04-17 23:27:19 +0300

    some escaped names

--- src/Glutinum.Converter/Utils/Naming.fs
+++ src/Glutinum.Converter/Utils/Naming.fs
@@ -8,16 +8,14 @@ let MODULE_PLACEHOLDER = "REPLACE_ME_WITH_MODULE_NAME"
 let private startWithDigit (name: string) : bool =
     name.Length > 0 && Char.IsDigit name.[0]
 
+let invalidchars = "|=-~&~$()#<> "
+
 let private escapeName (name: string) : string =
     if
-        name.Contains("-")
-        || name.Contains("$")
-        || name.Contains("#")
-        || name.Contains("<")
-        || name.Contains(">")
-        || name.Contains(" ")
-        || startWithDigit name
-        || Keywords.fsharp.Contains name
+        not (name.StartsWith("``"))
+        && (invalidchars |> String.exists (fun v -> name.IndexOf(v) <> -1)
+            || startWithDigit name
+            || Keywords.fsharp.Contains name)
     then
         $"``%s{name}``"
     else
@@ -44,18 +42,22 @@ let replaceAt (text: string) : string = text.Replace("@", "_AT_")
 type SanitizeNameResult = { Name: string; IsDifferent: bool }
 
 let sanitizeNameWithResult (name: string) : SanitizeNameResult =
-    let sanitizedName = name |> replaceDot |> replaceAt |> removeSurroundingQuotes
+    match name with
+    // not allowed in type name even with backticks
+    | "$type" -> { Name = "_Type"; IsDifferent = true }
+    | _ ->
+        let sanitizedName = name |> replaceDot |> replaceAt |> removeSurroundingQuotes
 
-    // Check if the name is different after sanitization
-    // This is used to check if the value is different from the default Fable computed value
-    // Especially useful for StringEnum values where we sometimes need to use [<CompiledValue(...)>]
-    // to provide a different value than the default Fable computed value from the name
-    let isDifferent = name <> sanitizedName
+        // Check if the name is different after sanitization
+        // This is used to check if the value is different from the default Fable computed value
+        // Especially useful for StringEnum values where we sometimes need to use [<CompiledValue(...)>]
+        // to provide a different value than the default Fable computed value from the name
+        let isDifferent = name <> sanitizedName
 
-    {
-        Name = sanitizedName |> escapeName
-        IsDifferent = isDifferent
-    }
+        {
+            Name = sanitizedName |> escapeName
+            IsDifferent = isDifferent
+        }
 
 let sanitizeName (name: string) =
     let result = sanitizeNameWithResult name
